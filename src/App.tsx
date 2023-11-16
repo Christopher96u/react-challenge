405494d67790c14d1f398e5135258674ed01f088
Christopher96u <christophermenendezmorales@gmail.com>
2023-11-16 15:07:52 +1100

    feat:add CountryProvider wrapper in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { CountryProvider } from "./context/CountryContext"
 import { Home } from "./pages/Home"
 
 function App() {
   return (
     <>
       <div className="bg-red-400 mx-8 md:mx-24 mt-5 md:mt-10">
-        <Home />
+        <CountryProvider>
+          <Home />
+        </CountryProvider>
       </div>
     </>
   )
